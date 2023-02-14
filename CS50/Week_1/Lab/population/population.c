ca03faff6caebbc26ed0a8c90fd46d7491775a52
manolisthebass <pos6r@hotmail.com>
2023-02-15 01:26:11 +0200

    basic_structure

---
+++ CS50/Week_1/Lab/population/population.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int startSize = get_start_size();
+
+    // TODO: Prompt for end size
+    int endSize = get_end_size();
+
+    // TODO: Calculate number of years until we reach threshold
+    int years = calculate_years(startSize, endSize);
+
+    // TODO: Print number of years
+    print_years(years);
+}
+
+int get_start_size(void)
+{
+
+}
+
+int get_end_size(void)
+{
+
+}
+
+int years(int start, int end)
+{
+
+}
+
+void print_years(int years)
+{
+    
+}
