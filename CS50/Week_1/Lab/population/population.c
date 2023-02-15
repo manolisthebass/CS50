69d3955d649c5865507d6c40ceea4499f7f138b9
manolisthebass <pos6r@hotmail.com>
2023-02-15 20:00:10 +0200

    working_code_no_unit tests

--- CS50/Week_1/Lab/population/population.c
+++ CS50/Week_1/Lab/population/population.c
@@ -1,37 +1,60 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_start_size(void);
+int get_end_size(int start);
+int calculate_years(int start, int end);
+void print_years(int years);
+
+
 int main(void)
 {
-    // TODO: Prompt for start size
+    // Prompt for start size
     int startSize = get_start_size();
 
-    // TODO: Prompt for end size
-    int endSize = get_end_size();
+    // Prompt for end size
+    int endSize = get_end_size(startSize);
 
-    // TODO: Calculate number of years until we reach threshold
+    // Calculate number of years until we reach threshold
     int years = calculate_years(startSize, endSize);
 
-    // TODO: Print number of years
+    // Print number of years
     print_years(years);
 }
 
 int get_start_size(void)
 {
-
+    int n;
+    do
+    {
+        n = get_int("Start population size: ");
+    } while (n < 9);
+    return n;
 }
 
-int get_end_size(void)
+int get_end_size(int start)
 {
-
+    int n;
+    do
+    {
+        n = get_int("End population size: ");
+    } while (n < start);
+    return n;
 }
 
-int years(int start, int end)
+int calculate_years(int start, int end)
 {
-
+    // The population rule: Say we have a population of n llamas. Each year, n / 3 new llamas are born, and n / 4 llamas pass away.
+    int n;  // n are the years requiered to get to the end population size
+    while (start < end)
+    {
+        n++;
+        start = start + (start / 3) - (start / 4);
+    }
+    return n;
 }
 
 void print_years(int years)
 {
-    
+    printf("The years needed are: %i\n", years);
 }
